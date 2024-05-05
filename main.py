a4007b2cb7e1ede0c9a3384f8f77bce12728e579
Team <yehudarothstein@gmail.com>
2024-05-05 17:10:51 +0300

    Commit Init

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("hi")
+import random
+computer = random.randint(1, 3)
+player = int(input("Enter 1 for Rock, 2 for Paper, 3 for Scissors: "))
+if computer == 1:
+    if player == 1:
+        print("Computer chose Rock, you chose Rock, it's a tie!")
+    elif player == 2:
+        print("Computer chose Rock, you chose Paper, you win!")
+    elif player == 3:
+        print("Computer chose Rock, you chose Scissors, you lose!")
+elif computer == 2:
+    if player == 1:
+        print("Computer chose Paper, you chose Rock, you lose!")
+    elif player == 2:
+        print("Computer chose Paper, you chose Paper, it's a tie!")
+    elif player == 3:
+        print("Computer chose Paper, you chose Scissors, you win!")
+else:
+    if player == 1:
+        print("Computer chose Scissors, you chose Rock, you win!")
+    elif player == 2:
+        print("Computer chose Scissors, you chose Paper, you lose!")
+    elif player == 3:
+        print("Computer chose Scissors, you chose Scissors, it's a tie!")
+
+# Path: main.py
