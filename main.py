babe24e9fa0206c75bc239a89affae8f66a41c2d
Team <yehudarothstein@gmail.com>
2024-05-12 19:33:02 +0300

    Commit Init

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
-import random
-computer = random.randint(1, 3)
-player = int(input("Enter 1 for Rock, 2 for Paper, 3 for Scissors: "))
-if computer == 1:
-    if player == 1:
-        print("Computer chose Rock, you chose Rock, it's a tie!")
-    elif player == 2:
-        print("Computer chose Rock, you chose Paper, you win!")
-    elif player == 3:
-        print("Computer chose Rock, you chose Scissors, you lose!")
-elif computer == 2:
-    if player == 1:
-        print("Computer chose Paper, you chose Rock, you lose!")
-    elif player == 2:
-        print("Computer chose Paper, you chose Paper, it's a tie!")
-    elif player == 3:
-        print("Computer chose Paper, you chose Scissors, you win!")
-else:
-    if player == 1:
-        print("Computer chose Scissors, you chose Rock, you win!")
-    elif player == 2:
-        print("Computer chose Scissors, you chose Paper, you lose!")
-    elif player == 3:
-        print("Computer chose Scissors, you chose Scissors, it's a tie!")
+import json
+from flask import Flask, request, render_template_string
 
-# Path: main.py
+app = Flask(__name__)
+
+@app.route('/')
+def form():
+    return render_template_string(open('index.html').read())
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    assigned_match_num = request.form.get('assigned_match_num')
+    usercode = request.form.get('usercode')
+    alliences = request.form.get('alliences')
+    place = request.form.get('place')
+
+    unicode_sign = (assigned_match_num+usercode+alliences+place)
+
+    kmatch = unicode_sign[0:2]
+    kusercode = unicode_sign[2:6]
+    kalliences = unicode_sign[6:7]
+    kplace = unicode_sign[7:8]
+
+    data = {
+        "Match": kmatch,
+        "User code": kusercode,
+        "Alliences": kalliences,
+        "Place": kplace
+    }
+
+    existing_data = []
+    try:
+        with open('data.json', 'r') as f:
+            existing_data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        pass
+
+    existing_data.append(data)
+
+    with open('data.json', 'w') as f:
+        json.dump(existing_data, f)
+
+    return f"Data stored in JSON file."
+
+if __name__ == '__main__':
+    app.run()
